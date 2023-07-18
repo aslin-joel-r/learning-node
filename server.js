3152c8899e32bcd00330c31787ca1bb8e4972383
aslin-r <aslinjoelr123@gmail.com>
2023-07-18 12:00:44 +0530

    used the .env to store the global variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express =require ('express');
-
+const dotenv =require("dotenv").config();
 const app=express();
-const port=5000;
+const port=process.env.PORT || 3000;
 
 
 app.listen(port,()=>{
