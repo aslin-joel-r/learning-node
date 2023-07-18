c9551cbce46c4649031ec98165495d4720af6572
aslin-r <aslinjoelr123@gmail.com>
2023-07-18 12:11:51 +0530

    sucessfully runned the server with response

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const dotenv =require("dotenv").config();
 const app=express();
 const port=process.env.PORT || 3000;
 
+app.get('/api/contacts',(req,res)=>{
+  res.send('Get all Contacts');
+})
 
 app.listen(port,()=>{
-  console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`);
 })
