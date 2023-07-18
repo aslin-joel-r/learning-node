5644b7dc5ad6792d731609c31d1c75391742c7da
aslin-r <aslinjoelr123@gmail.com>
2023-07-18 12:46:07 +0530

    finished the router setup

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,6 +4,22 @@ const router=express.Router();
 router.route('/').get((req, res) => {
     res.status(200).json({ msg: 'Get all the Contacts' });
   });
+
+  router.route('/').post((req, res) => {
+    res.status(200).json({ msg: 'Create a contacts' });
+  });
+
+  router.route('/:id').get((req, res) => {
+    res.status(200).json({ msg: `Get contact for ${req.params.id}` });
+  });
+
+  router.route('/:id').put((req, res) => {
+    res.status(200).json({ msg: `update the contact ${req.params.id}` });
+  });
+
+  router.route('/:id').delete((req, res) => {
+    res.status(200).json({ msg: `Delete the contact ${req.params.id}` });
+  });
   
 
 module.exports=router;
