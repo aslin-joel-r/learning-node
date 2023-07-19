7c061180aa0e62b16db56afce6cc5de3c3cb3b3d
aslin-r <aslinjoelr123@gmail.com>
2023-07-19 10:37:38 +0530

    solved the error in the previous code

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,49 +1,51 @@
-const asyncHandler=require("express-async-handler")
+const asyncHandler = require("express-async-handler");
+
 //@desc get all contacts
 //@route GET /api/contacts
-//@acess public
-
-const getContacts=asyncHandler(req, res) => {
-    res.status(200).json({ msg: 'Get all the Contacts' });
-  };
+//@access public
+const getContacts = asyncHandler((req, res) => {
+  res.status(200).json({ msg: 'Get all the Contacts' });
+});
 
 //@desc Create new contact
 //@route POST /api/contacts
-//@acess public
-
-const createContact=asyncHandler(req, res) => {
-  console.log("The request body is ",req.body);
-  const{name,email,phone}=req.body;
-  if(!name || !email || !phone ){
+//@access public
+const createContact = asyncHandler((req, res) => {
+  console.log("The request body is ", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
     res.status(400);
     throw new Error("please enter all the fields");
   }
 
   res.status(200).json({ msg: 'Create a contact' });
-};
+});
 
 //@desc Get contact
 //@route GET /api/contacts/:id
-//@acess public
-
-const getContact=asyncHandler(req, res) => {
+//@access public
+const getContact = asyncHandler((req, res) => {
   res.status(200).json({ msg: `Get contact for ${req.params.id}` });
-}
+});
+
 //@desc update contacts
 //@route PUT /api/contacts/:id
-//@acess public
-
-const updateContact=asyncHandler(req, res) => {
+//@access public
+const updateContact = asyncHandler((req, res) => {
   res.status(200).json({ msg: `update the contact ${req.params.id}` });
-};
+});
 
 //@desc Delete contacts
 //@route DELETE /api/contacts/:id
-//@acess public
-
-const deleteContact=asyncHandler(req, res) => {
+//@access public
+const deleteContact = asyncHandler((req, res) => {
   res.status(200).json({ msg: `Delete the contact ${req.params.id}` });
+});
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact
 };
-
-
-  module.exports={getContacts,createContact,getContact,updateContact,deleteContact}
