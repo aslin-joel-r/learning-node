3c9ddaa880e9572552ae587e71a47ec5da36611d
aslin-r <aslinjoelr123@gmail.com>
2023-07-19 14:47:41 +0530

    updated

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-const express =require ('express');
-const errorHandler = require('./middleware/errorhandler');
-const dotenv =require("dotenv").config();
-const app=express();
-const port=process.env.PORT || 3000;
+// create a basic express program with a server and the response message with hello
 
-app.use(express.json());
-app.use("/api/contacts",require("./routes/contactRoutes"));
-app.use(errorHandler);
+const express = require('express');
+const mongodb= require('mongodb');
+const app = express();
 
-app.listen(port,()=>{
-  console.log(`Server is running on port ${port}`);
-})
+const url= 'mongodb://localhost:27017';
+
+const MongoClient = mongodb.MongoClient;
+
+MongoClient.connect(url, function(err, db) {
+  if (err) {
+    console.log('Error in connecting to database');
+  };
+  console.log("Database created!");
+  db.close();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+}
+)
+
+// connect with database mongodb
+app.listen(5001, () => {
+  console.log('Server is running on port 5001');
+}
+)
