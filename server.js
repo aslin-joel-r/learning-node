0d2b1a735633a7645627589909cb1d28b9dcda4a
aslin-r <aslinjoelr123@gmail.com>
2023-07-20 10:46:38 +0530

    updated

--- server.js
+++ server.js
@@ -3,21 +3,30 @@ const { MongoClient } = require("mongodb");
 // Connection URI for the MongoDB server running on localhost, default port 27017
 const uri = "mongodb://localhost:27017/";
 
-// Name of the  database
+// Name of the new database
 const databaseName = "All_Glory_to_Jesus";
 
+
+const collectionData = { name: "joel" };
+
+// Data to be updated
+const updateedData={name:"I am  updated"}
 // Function to connect to MongoDB, create a database, and add a collection
 async function createDatabaseAndCollection() {
   const client = new MongoClient(uri);
-  console.log(client)
+
   try {
     // Connect to MongoDB
     await client.connect();
 
-    
-   await client.db(databaseName).dropDatabase();
+    // Get a reference to the new database
+    const database = client.db(databaseName);
 
-    console.log(`${databaseName} dropped successfully!`);
+    // updating the  data
+    const collection = database.collection("mycollection");
+    await collection.updateOne(collectionData, { $set:updateedData});
+
+    console.log("Database and collection updated successfully!");
   } catch (error) {
     console.error("Error:", error);
   } finally {
